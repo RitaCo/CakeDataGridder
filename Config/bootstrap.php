766fe7ca65cfed1c622b5bdfd54fd24bf3c94ee6
zoghal <saleh.souzanchi@gmail.com>
2014-04-09 19:23:56 +0430

    up

--- Config/bootstrap.php
+++ Config/bootstrap.php
@@ -3,12 +3,12 @@
 
 
 RitaEvent::on('Controller.beforeInit',function(CakeEvent $event){
-	if ( Rita::debug()) {
+	
    		
 		$event->subject()->components[] = 'DataGridder.DataGrid';
 		$event->subject()->helpers[] = 'DataGridder.DataGrid';
 		 
-    } 
+     
 });
 
 
